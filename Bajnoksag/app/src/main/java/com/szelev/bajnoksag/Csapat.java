ce9a004ad4ace6a7b52febf1dfda1f7a8a670d13
Szemerédi Levente <levente.szemeredi@yahoo.com>
2016-12-06 18:38:51 +0100

    Jatekosok felvetele

---
+++ Bajnoksag/app/src/main/java/com/szelev/bajnoksag/Csapat.java
@@ -0,0 +1,49 @@
+package com.szelev.bajnoksag;
+
+/**
+ * Created by Levente on 2016.12.06..
+ */
+
+public class Csapat {
+
+    private int ID;
+    private String nev;
+    private int suly;
+
+    public Csapat() {
+        ID      = -1;
+        nev     = "";
+        suly    = 0;
+    }
+
+    public Csapat(int ID, String nev, int suly) {
+        this.ID     = ID;
+        this.nev    = nev;
+        this.suly   = suly;
+    }
+
+    public void setID(int ID) {
+        this.ID     = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setNev(String nev) {
+        this.nev    = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setSuly(int suly) {
+        this.suly   = suly;
+    }
+
+    public int getSuly() {
+        return suly;
+    }
+
+}

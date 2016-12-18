85e5c257d04262396e250190874706a1f54d6268
Szemerédi Levente <levente.szemeredi@yahoo.com>
2016-12-18 20:57:28 +0100

    Kormerkozeses mod tovabbfejlesztese, kiertekeles megkezdese

--- Bajnoksag/app/src/main/java/com/szelev/bajnoksag/Kormerkozesek.java
+++ Bajnoksag/app/src/main/java/com/szelev/bajnoksag/Kormerkozesek.java
@@ -1,35 +1,53 @@
 package com.szelev.bajnoksag;
 
 import android.graphics.Color;
+import android.opengl.EGLExt;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Levente on 2016.12.06..
  */
 
-public class Kormerkozesek extends AppCompatActivity {
+public class Kormerkozesek extends AppCompatActivity implements View.OnClickListener{
 
-    private TableLayout merkozesTabla;
-    private MainActivity mainAct;
+    private TableLayout     merkozesTabla;
+    private MainActivity    mainAct;
+    private Spinner         csapatok1, csapatok2;
+    private Button          mentes;
+    private TextView        er1, er2;
 
     public static ArrayList<ArrayList<Eredmeny>> eredmenyek;
     public Kormerkozesek(MainActivity mainAct)
     {
         this.mainAct = mainAct;
 
-        merkozesTabla = (TableLayout)   (mainAct.findViewById(R.id.table_main));
+        merkozesTabla   = (TableLayout) (mainAct.findViewById(R.id.table_main));
+        csapatok1       = (Spinner)     (mainAct.findViewById(R.id.spinner1));
+        csapatok2       = (Spinner)     (mainAct.findViewById(R.id.spinner2));
+        mentes          = (Button)      (mainAct.findViewById(R.id.button));
+        er1             = (TextView)    (mainAct.findViewById(R.id.editText));
+        er2             = (TextView)    (mainAct.findViewById(R.id.editText2));
 
-        initTable();
+        mentes.setOnClickListener(this);
+
+        initT();
     }
 
-    private void initTable()
+    private void refreshT()
     {
+        merkozesTabla.removeAllViews();
+
         TableRow tr = new TableRow(mainAct);
         TextView tv = new TextView(mainAct);
         tv.setText("");
@@ -72,6 +90,27 @@ public class Kormerkozesek extends AppCompatActivity {
             merkozesTabla.addView(tr);
         }
 
+
+    }
+
+    private void initT()
+    {
+        refreshT();
+
+        er1.setText("0");
+        er2.setText("0");
+
+        List<String> list = new ArrayList<String>();
+        list.add("");
+
+        for(int i=0; i<MainActivity.csapatok.size(); i++)
+        {
+            list.add(MainActivity.csapatok.get(i).getNev());
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(mainAct, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        csapatok1.setAdapter(dataAdapter);
+        csapatok2.setAdapter(dataAdapter);
     }
 
     public static void initEredmenyek()
@@ -90,5 +129,31 @@ public class Kormerkozesek extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        if(mainAct.findViewById(R.id.button).equals(v))
+        {
+            int i, j;
 
+            i = (int) csapatok1.getSelectedItemId()-1;
+            j = (int) csapatok2.getSelectedItemId()-1;
+
+            csapatok1.setSelection(0);
+            csapatok2.setSelection(0);
+
+            if(i!=j && i!=-1 && j!=-1)
+            {
+                eredmenyek.get(i).get(j).setEredmeny(Integer.parseInt(er1.getText().toString()), Integer.parseInt(er2.getText().toString()));
+                eredmenyek.get(j).get(i).setEredmeny(Integer.parseInt(er2.getText().toString()), Integer.parseInt(er1.getText().toString()));
+            }
+
+            refreshT();
+
+            er1.setText("0");
+            er2.setText("0");
+        } else {
+            MainActivity.activity_number = 2;
+            mainAct.create();
+        }
+    }
 }
